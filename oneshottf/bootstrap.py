507801dd1859caa7c596c93661839ade84e03a57
2sang <sangsulee92@gmail.com>
2018-09-28 22:42:08 +0900

    Add three dockerfiles

--- oneshottf/bootstrap.py
+++ oneshottf/bootstrap.py
@@ -1,5 +1,8 @@
+import os
 import yaml
 import docker
+CONFIG_FILE = 'conf.yaml'
+CONFIG_PATH = os.path.join(os.getcwd(), CONFIG_FILE)
 
 def init_client():
     return docker.from_env()
@@ -9,24 +12,24 @@ def check_requirements():
     return True
 
 
-def build_image_from_config(conf):
+def build_image_from_config(conf_dict):
     pass
 
 
-def load_config():
-    with open('conf.yaml', 'r') as f:
+def load_config(config_path=CONFIG_PATH):
+    with open(config_path, 'r') as f:
         config = yaml.load(f)
     return config
         
 
 def main():
     conf_dict = load_config()
-    client = init_client(conf_dict)
+    client = docker.from_env()
     if not check_requirements():
         print("check requirement failed, exit.")
         return
     
-    image = build_image_from_config()
+    image = build_image_from_config(conf_dict)
     
         
 if __name__ == "__main__":
--- tests/test_oneshot_tf.py
+++ tests/test_oneshot_tf.py
@@ -1,14 +1,11 @@
-import sys
 import pytest
+import sys
+print("sys.path: {}".format(sys.path))
 from oneshottf import bootstrap as bs
 
-print("sys.path: {}".format(sys.path))
-
-def inc(x):
-    return x + 1
-
-def test_answer():
-    assert inc(3) == 4
+def test_load_config():
+    conf = bs.load_config()
+    assert type(conf) == dict
     
 def test_check_requirements():
     assert bs.check_requirements()
