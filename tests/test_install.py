12fe973979a04fe043558a2752296b747d9831b9
2sang <sangsulee92@gmail.com>
2018-10-01 23:08:56 +0900

    Add 3 tc, refine install

--- tests/test_install.py
+++ tests/test_install.py
@@ -1,4 +1,31 @@
+import os
 import pytest
-import tfgpu
+import tfgpu.install as install
+from tfgpu.install import RUNPATH
 
 
+def test_load_conf():
+    d = install.load_conf()
+    print("d: {}".format(d))
+    assert type(d) == dict
+
+
+def test_check_prerequisite():
+    assert install.check_prerequisites()
+
+
+def test_set_shell_command():
+    assert not os.path.exists(os.path.expanduser('~/.tfgpu'))
+    installed = install.set_shell_commands()
+    assert installed
+    assert os.path.exists('~/.tfgpu')
+    with open(os.path.expanduser('~/.bashrc'), 'r') as tfgpu:
+        lines = tfgpu.readlines()
+        assert 'source ~/.tfgpu\n' in lines
+
+    # Teardown
+    os.remove('~/.tfgpu')
+    lines.remove('source ~/.tfgpu\n')
+    with open(os.path.expanduser('~/.bashrc'), 'w') as brc:
+        for line in lines:
+            brc.write(line)
--- tfgpu/install.py
+++ tfgpu/install.py
@@ -5,34 +5,44 @@ from __future__ import print_function
 import os
 import docker
 import yaml
-from subprocess
+import subprocess
+
+
+# These need test too
+HOST_PYTHON_VERSION = 'python3'
+RUNFILE = 'run.py'
+INSTALL_FILEPATH = os.path.realpath(__file__)
+RUNPATH = os.path.join(os.path.dirname(INSTALL_FILEPATH), RUNFILE)
 
 
 def check_prerequisites():
-    # tfgpu dependencies(pip2, pip3)
+    # TODO: tfgpu dependencies(pip2, pip3)
+    if False:
+        raise PrerequisiteNotSatisfied
 
     # nvidia driver dependencies
-
     return True
 
 
 def load_conf(yaml_path='./conf.yaml'):
     with open(yaml_path, 'r') as f:
-        return yaml.load(f)
+        return dict(yaml.load(f))
 
 
 def set_shell_commands():
-    with open('~/.tfgpu', 'w') as tfgpu:
-        tfgpu.write('alias tfgpu="python3 ' + os.path.realpath(__file__) + '"')
-    with open('~/.bashrc', 'a') as brc:
-        brc.write('source ')
-    call(['source', '~/.bashrc'])
+    with open(os.path.expanduser('~/.tfgpu'), 'w') as tfgpu:
+        tfgpu.write('alias tfgpu="python3 ' + RUNPATH + '"')
+    with open(os.path.expanduser('~/.bashrc'), 'a') as brc:
+        brc.write('source ~/.tfgpu')
+    subprocess.call(['source', '~/.bashrc'])
 
 
-def install():
-    if os.path.exists('~/.tfgpu'):
-        return True
+def install(conf):
+    if os.path.exists(os.path.expanduser('~/.tfgpu')):
+        print("tfgpu exists. exit")
+        return False
     set_shell_commands()
+    return True
 
 
 def main():
@@ -40,9 +50,8 @@ def main():
     if not (check_prerequisites()):
         print("prerequisites not satistied")
         return
-
     conf = load_conf('conf.yaml')
-    install()
+    install(conf)
 
 
 class PrerequisiteNotSatisfied(Exception):
