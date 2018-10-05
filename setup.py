d6cfcb4064de18287be6659760066985cfad83d6
2sang <sangsulee92@gmail.com>
2018-10-05 21:30:34 +0900

    Seems like the install process is completely garbage code...

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Tfgpu",
+    version="0.1",
+    packages=find_packages(),
+    entry_points={"console_scripts": ["tfgpu=tfgpu.command_line:main"]},
+
+    install_requires=[
+        'docker>=3.4',
+        'pyyaml>=3.1',
+        'absl-py>=0.5'
+    ],
+
+    author="Sang Su Lee",
+    author_email="sangsulee92@gmail.com",
+    description="Yet another docker command line tool for tensorflow",
+    license="MIT",
+    keywords="docker tensorflow tensorflow-gpu",
+)
--- tfgpu/cli/_ls.py
+++ tfgpu/cli/_ls.py
@@ -9,5 +9,9 @@ FLAGS = flags.FLAGS
 def main(argv, client):
     FLAGS(argv)
     cs = client.containers.list(all=True)
+    if not cs:
+        print("No container running.")
+        return;
+    print("CID  image\tcontainer_nickname")
     for container in cs:
-        print(container.id, container.name)
+        print(container.id[:4], container.image, container.name)
